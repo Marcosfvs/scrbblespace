c1b5b6c60327b08f89a2862aea2768f655e44c99
Marcos Silva <marcos.fvsilva11@hotmail.com>
2025-01-20 20:24:36 +0100

    Aula FireBase

--- app/src/main/java/com/devspace/scribblespace/common/remote/RemoteDataSource.kt
+++ app/src/main/java/com/devspace/scribblespace/common/remote/RemoteDataSource.kt
@@ -4,6 +4,7 @@ import android.provider.ContactsContract.CommonDataKinds.Note
 import com.devspace.scribblespace.common.model.NoteData
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withTimeout
 import kotlin.Exception
 
 private const val NOTE_COLLECTION = "notes"
@@ -14,8 +15,25 @@ class RemoteDataSource private constructor(
 ) {
 
     suspend fun addNote(title: String, description: String): Result<String> {
-        return TODO()
+        return try {
+
+            val noteMap = hashMapOf(
+                "title" to title,
+                "description" to description
+            )
+
+            val addedDocument = withTimeout(2000) {
+                dataBase.collection(NOTE_COLLECTION)
+                    .add(noteMap)
+                    .await()
+            }
+            Result.success(addedDocument.id)
+
+        } catch (ex: Exception) {
+            Result.failure(ex)
+        }
     }
+
 
     // retorno de função pode apenas ter um tipo de retorno
     // Com o result pode ter dois tipos, sucesso ou falha
@@ -54,7 +72,18 @@ class RemoteDataSource private constructor(
     }
 
     suspend fun deleteNote(id: String): Result<Unit> {
-        return TODO()
+        return try {
+            withTimeout(2000){
+            dataBase.collection(NOTE_COLLECTION)
+                .document(id)
+                .delete()
+                .await()
+            Result.success(Unit)
+            }
+
+        } catch (ex: Exception){
+            Result.failure(ex)
+        }
     }
 
     companion object {
